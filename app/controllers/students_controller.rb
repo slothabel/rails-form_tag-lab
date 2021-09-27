b3f78e15219b9065480a9dc0d3bac2727aa2e813
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-27 14:01:56 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,16 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  #params
+# => <ActionController::Parameters {"controller"=>"students", "action"=>"new"} permitted: false>
+
+  def new
+binding.pry
+    @student = Student.create(params[:student])
+
+  end
+
+  def create
+  end
+  
 end
